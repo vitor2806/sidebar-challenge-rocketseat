778c4e79038252a1994788248e7f66e9b5e83523
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-21 14:47:13 -0300

    add initial script behavior

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
 const buttonCtrl = document.querySelector('#hamburger')
 const aside = document.querySelector('aside')
+const menuHeader = document.querySelector('.menu-header')
 
 buttonCtrl.addEventListener('click', () => {
-    aside.style.width === 'unset'
+    const isCollapsed = aside.style.width === 'unset'
+
+    if (isCollapsed) {
+        aside.style.width = '15%'
+    } else {
+        aside.style.width = 'unset'
+    }
 })
