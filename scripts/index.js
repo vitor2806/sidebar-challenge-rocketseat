56c93cb5c7cb56551380c5d75552eec8058f84fb
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-21 15:31:11 -0300

    add javascript for hiding sidebar

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,27 @@
 const buttonCtrl = document.querySelector('#hamburger')
 const aside = document.querySelector('aside')
 const menuHeader = document.querySelector('.menu-header')
+const profilePic = document.querySelector('.profile-pic')
+const menuLogo = document.querySelector('#logo')
+const itemDesc = document.querySelectorAll('.item-desc')
 
 buttonCtrl.addEventListener('click', () => {
-    const isCollapsed = aside.style.width === 'unset'
-
-    if (isCollapsed) {
-        aside.style.width = '15%'
+    const isCollapsed = aside.classList === 'collapse'
+    
+    if (!isCollapsed) {
+        // aside.style.width = 'unset'
+        aside.classList.add('collapse')
+        profilePic.classList.add('show-hide')
+        itemDesc.forEach((span) => {
+            span.classList.add('show-hide')
+        })
+        menuLogo.classList.add('show-hide')
     } else {
-        aside.style.width = 'unset'
+        aside.classList.remove('collapse')
+        profilePic.classList.remove('show-hide')
+        itemDesc.forEach((span) => {
+            span.classList.remove('show-hide')
+        })
+        menuLogo.classList.remove('show-hide')
     }
 })
