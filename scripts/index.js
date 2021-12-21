32329733634484b92e0e7f131cc867fdc39f0b9b
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-21 14:40:58 -0300

    add sidebar style

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const buttonCtrl = document.querySelector('#hamburger')
+const aside = document.querySelector('aside')
+
+buttonCtrl.addEventListener('click', () => {
+    aside.style.width === 'unset'
+})
