55ede19916a8acd3fe444e048945e95c1dc8c106
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-25 10:59:18 -0300

    add onclick focus for search button

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,17 @@
-const menuBtn = document.querySelector('#hamburger')
-const aside = document.querySelector('aside')
+const menuBtn = document.querySelector('#hamburger');
+const aside = document.querySelector('aside');
+const searchButton = document.querySelector('.search');
+const searchField = document.querySelector('#search');
 
 menuBtn.addEventListener('click', () => {
-    aside.classList.toggle('open')
-})
+	aside.classList.toggle('open');
+});
+
+searchButton.addEventListener('click', () => {
+	isOpen = aside.classList.value = 'open';
+
+    searchField.focus();
+	if (!isOpen) {
+		aside.classList.toggle('open');
+	}
+});
